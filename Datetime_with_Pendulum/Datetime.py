82ce73bae45b43c2b06a8172168d58288a7db3ac
bruno_gaddini <bruno_gaddini@hotmail.com>
2022-11-29 23:02:12 -0300

    Initial commit

--- Datetime_with_Pendulum/Datetime.py
+++ Datetime_with_Pendulum/Datetime.py
@@ -10,3 +10,6 @@ common_parsed = parse('1996-12-19T16:39:57-08:00')
 print(common_parsed)
 
 print(common_parsed.is_leap_year())
+
+# https://www.youtube.com/watch?v=iy3fGZEK8f4&list=PLOQgLBuj2-3K1hb7XgkGPb4S9YNIeHsPk&index=15
+# 17:55
