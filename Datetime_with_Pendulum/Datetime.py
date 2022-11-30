1bd26bfe9857fd12e49721e8d8ba4b726222c515
bruno_gaddini <bruno_gaddini@hotmail.com>
2022-11-30 00:01:04 -0300

    Update Datetime.py

--- Datetime_with_Pendulum/Datetime.py
+++ Datetime_with_Pendulum/Datetime.py
@@ -1,1 +1,12 @@
-a =1
+from pendulum import parse, datetime
+
+dt = datetime(2020, 9, 7)
+print(dt)
+
+simplified_parsed = parse('20-09-7', strict=False)
+print(simplified_parsed)
+
+common_parsed = parse('1996-12-19T16:39:57-08:00')
+print(common_parsed)
+
+print(common_parsed.is_leap_year())
